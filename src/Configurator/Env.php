55f765c13da221eb84632b992193c4c2e79a5da4
enjoys <dealy.info@gmail.com>
2025-04-17 18:44:18 +0300

    Env Configurator

--- src/Configurator/Env.php
+++ src/Configurator/Env.php
@@ -8,8 +8,25 @@ use Composer\IO\IOInterface;
 class Env extends AbstractConfigurator
 {
 
+    /**
+     * @throws \Exception
+     */
     public function process(): void
     {
-
+        $envPath = getenv('ROOT_PATH').'/.env.dist';
+        $dotenvWriter = new \Enjoys\DotenvWriter\DotenvWriter($envPath);
+        $dotenvWriter->addLine(
+            new \Enjoys\Dotenv\Parser\Lines\CommentLine('sdfghj')
+        );
+        foreach ($this->options as $key => $value) {
+            $this->io->write(sprintf('<comment>Write ENV: %s</comment>', $key));
+            $dotenvWriter->addLine(
+                new \Enjoys\Dotenv\Parser\Lines\EnvLine(
+                    new \Enjoys\Dotenv\Parser\Env\Key($key),
+                    new \Enjoys\Dotenv\Parser\Env\Value($value)
+                )
+            );
+        }
+        $dotenvWriter->save();
     }
 }
