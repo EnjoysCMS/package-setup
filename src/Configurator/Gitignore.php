f38d54bfe09664b7ee45a475115ad4135a54cff2
enjoys <dealy.info@gmail.com>
2025-04-25 14:09:35 +0300

    Added gitignore handler

--- src/Configurator/Gitignore.php
+++ src/Configurator/Gitignore.php
@@ -2,10 +2,21 @@
 
 namespace Enjoyscms\PackageSetup\Configurator;
 
+use Enjoyscms\PackageSetup\Utils\GitignoreManage;
+
 class Gitignore extends AbstractConfigurator
 {
 
     public function process(): void
     {
+        $gitignoreManage = new GitignoreManage(
+            gitignoreFile: $this->composer->getConfig()->get('root-path') . '/.gitignore'
+        );
+
+        $this->io->write('<comment>Writing to .gitignore:</comment>');
+        foreach ($this->options as $value) {
+            $gitignoreManage->add($value);
+        }
+        $gitignoreManage->save();
     }
 }
---
+++ src/Utils/GitignoreManage.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Enjoyscms\PackageSetup\Utils;
+
+use function Enjoys\FileSystem\writeFile;
+
+class GitignoreManage
+{
+
+    private array $structure;
+
+    public function __construct(private string $gitignoreFile)
+    {
+        if (!file_exists($gitignoreFile)) {
+            writeFile($gitignoreFile, '', 'w+');
+        }
+
+        $this->structure = $this->parseStructure(file_get_contents($gitignoreFile) ?: '');
+    }
+
+    public function add(string $value): GitignoreManage
+    {
+        if (preg_match("/\R/u", $value) !== 0) {
+            throw new \InvalidArgumentException('The input string should not consist of several lines.');
+        }
+
+        if (!in_array($value, $this->structure, true)) {
+            $this->structure[] = $value;
+        }
+
+        return $this;
+    }
+
+    public function save(): void
+    {
+        file_put_contents($this->gitignoreFile, implode("\n", $this->structure));
+    }
+
+    private function parseStructure(string $content): array
+    {
+        $split = preg_split("/\R/u", $content);
+        return ($split === false) ? [] : $split;
+    }
+}
